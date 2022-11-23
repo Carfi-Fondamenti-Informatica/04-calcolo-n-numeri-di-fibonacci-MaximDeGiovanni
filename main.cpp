07e4bb0ff937dff7751114d496bb3f670a90538b
MaximDeGiovanni <114994423+MaximDeGiovanni@users.noreply.github.com>
2022-11-23 15:20:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
+int Numero, a=1, b=1, c;
+cin>>Numero;
+  if (Numero<2) {
+        cout << "errore" << endl;
+
+ }else {
+        cout<<a<<endl;
+        cout<<b<<endl;
+        for (int i =2; i < Numero; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+            cout << c << endl;
+        }
+    }
    return 0;
 }
